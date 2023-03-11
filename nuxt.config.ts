9affef7b6464d7cbbb54e86e91271f2cb48b2e6a
Lsscr <57869443+Lsscr@users.noreply.github.com>
2023-03-11 15:04:22 +0800

    chore(Nuxt): 配置修改

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: "vercel",
+    server: {
+      // 默认localhost 只能本地访问
+      // 若想要对外访问，要配置为 0.0.0.0
+      host: "0.0.0.0",
+      // nuxtjs 默认 3000
+      // vercel 默认80
+      port: 80,
+    },
+  },
   typescript: { shim: false },
   modules: ["@nuxtjs/tailwindcss", "@huntersofbook/naive-ui-nuxt"],
 });
